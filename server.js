3d05975f5b51e2aacc8246a8cb96f0659e2fac9a
HiddenDev <ss0823903@gmail.com>
2026-09-09 22:32:55 +0530

    running server with express

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-let http = require("http");
+// let http = require("http");
 
-let server = http.createServer((req, res) => {
-  console.log("hey");
-  res.end("Response found Successfully");
-});
+// let server = http.createServer((req, res) => {
+//   console.log("hey");
+//   res.end("Response found Successfully");
+// });
 
-server.listen(3000, () => {
+// server.listen(3000, () => {
+//   console.log("Server is running on 3000 port");
+// });
+
+
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Response found Successfully");
+})
+
+app.listen(3000, () => {
   console.log("Server is running on 3000 port");
 });
